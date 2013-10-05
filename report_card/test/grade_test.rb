e83cf343399b998767b69cf7a639c43a108d4ce7
Katrina Owen <katrina.owen@gmail.com>
2013-10-04 19:32:33 -0600

    Add more data to Report Card.

--- report_card/test/grade_test.rb
+++ report_card/test/grade_test.rb
@@ -15,18 +15,129 @@ class GradeTest < Minitest::Test
     assert_equal "Programming in Ruby", grade.subject
   end
 
-  def test_percentage
+  def test_score
     skip
-    grade = Grade.new(percentage: "98")
-    assert_equal "98", grade.percentage
+    grade = Grade.new(score: "98")
+    assert_equal 98, grade.score
   end
 
-  def test_all_the_things
+  def test_a
     skip
-    data = {subject: "Building Compilers", percentage: "76"}
-    grade = Grade.new(data)
-    assert_equal "Building Compilers", grade.subject
-    assert_equal "76", grade.percentage
+    (94..100).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("A", grade)
+      assert_grade_point(4.0, grade)
+    end
   end
+
+  def test_a_minus
+    skip
+    (90..93).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("A-", grade)
+      assert_grade_point(3.7, grade)
+    end
+  end
+
+  def test_b_plus
+    skip
+    (87..89).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("B+", grade)
+      assert_grade_point(3.3, grade)
+    end
+  end
+
+  def test_b
+    skip
+    (83..86).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("B", grade)
+      assert_grade_point(3.0, grade)
+    end
+  end
+
+  def test_b_minus
+    skip
+    (80..82).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("B-", grade)
+      assert_grade_point(2.7, grade)
+    end
+  end
+
+  def test_c_plus
+    skip
+    (77..79).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("C+", grade)
+      assert_grade_point(2.3, grade)
+    end
+  end
+
+  def test_c
+    skip
+    (73..76).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("C", grade)
+      assert_grade_point(2.0, grade)
+    end
+  end
+
+  def test_c_minus
+    skip
+    (70..72).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("C-", grade)
+      assert_grade_point(1.7, grade)
+    end
+  end
+
+  def test_d_plus
+    skip
+    (67..69).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("D+", grade)
+      assert_grade_point(1.3, grade)
+    end
+  end
+
+  def test_d
+    skip
+    (63..66).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("D", grade)
+      assert_grade_point(1.0, grade)
+    end
+  end
+
+  def test_d_minus
+    skip
+    (60..62).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("D-", grade)
+      assert_grade_point(0.7, grade)
+    end
+  end
+
+  def test_f
+    skip
+    (0..59).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("F", grade)
+      assert_grade_point(0.0, grade)
+    end
+  end
+
+  private
+
+  def assert_grade(letter, grade)
+    assert_equal letter, grade.letter, "#{grade.score}% should be #{letter}"
+  end
+
+  def assert_grade_point(grade_point, grade)
+    assert_equal grade_point, grade.point, "#{grade.score}% should be #{grade_point}"
+  end
+
 end
 
